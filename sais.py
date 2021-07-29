add9b49a9424851b6dd3270ffed1aec6f87f32c4
yazzukas <ambus.sten@gmail.com>
2021-07-29 07:21:14 +0300

    file opening bug fix, new result files

--- sais.py
+++ sais.py
@@ -9,19 +9,20 @@ session = HTMLSession()
 r = session.get('https://www.sais.ee/PublicInfo/Rankings')
 koolid = r.html.find('body > div.body > div.container > div', first=True)
 
-for kooli_link in koolid.absolute_links:
-    r = session.get(kooli_link)
-    kooli_nimi = r.html.find('body > div.body > div.container > div > h1', first=True).text
-    eriala_vastuvõtt = r.html.find('body > div.body > div.container > div > table:nth-child(3) > tbody', first=True)
-    if eriala_vastuvõtt is not None:
-        for link in eriala_vastuvõtt.absolute_links:
-            if "Accepted" in link:
-                r = session.get(link)
-                eriala_nimi = r.html.find('body > div.body > div.container > div > h1', first=True).text
-                vastuvõetud_õpilased = r.html.find('body > div.body > div.container > div > table > tbody', first=True).text.splitlines()
-                with open(str(date.today()) + ".txt", "w") as fail:
+with open(str(date.today()) + ".txt", "w", encoding='utf-8') as fail:
+    for kooli_link in koolid.absolute_links:
+        r = session.get(kooli_link)
+        kooli_nimi = r.html.find('body > div.body > div.container > div > h1', first=True).text
+        eriala_vastuvõtt = r.html.find('body > div.body > div.container > div > table:nth-child(3) > tbody', first=True)
+        if eriala_vastuvõtt is not None:
+            for link in eriala_vastuvõtt.absolute_links:
+                if "Accepted" in link:
+                    r = session.get(link)
+                    eriala_nimi = r.html.find('body > div.body > div.container > div > h1', first=True).text
+                    vastuvõetud_õpilased = r.html.find('body > div.body > div.container > div > table > tbody', first=True).text.splitlines()
                     for vastuvõetud_õpilane in vastuvõetud_õpilased:
                         if "Kandidaat ei avalikusta nime" not in vastuvõetud_õpilane:
                             fail.write(vastuvõetud_õpilane + "; " + kooli_nimi + "; " + eriala_nimi + "\n")
+                            #print(vastuvõetud_õpilane + "; " + kooli_nimi + "; " + eriala_nimi)
 toc = time.perf_counter()
 print(toc - tic)
